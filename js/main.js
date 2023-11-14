ad93f3b0c44091b3b4f99c68beeeec6a2e7d2ef0
Полина <polina.alekseevna0922@outlook.com>
2023-11-14 13:29:08 +0500

    отрисовка миниатюр

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import { generatePhotosArray } from './data.js';
+//import { generatePhotosArray } from './data.js';
+import { createPictureElement } from './pictureThumbnailsRenderer.js';
 
-const photos = generatePhotosArray();
+const photos = createPictureElement();
 
 // eslint-disable-next-line no-console
 console.log(photos);
