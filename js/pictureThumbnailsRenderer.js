39003d5e5ff6c2b2f8a3f707225f50aa07c85581
Полина <polina.alekseevna0922@outlook.com>
2023-11-14 13:23:43 +0500

    Отрисуй меня полностью

--- js/pictureThumbnailsRenderer.js
+++ js/pictureThumbnailsRenderer.js
@@ -1,47 +1,29 @@
-// Модуль, отвечающий за отрисовку миниатюр
-const picturesModule = (() => {
-  const picturesData = [
-    {
-      url: 'url_для_изображения_1',
-      description: 'описание_изображения_1',
-      likes: 10,
-      comments: 5
-    },
-    {
-      url: 'url_для_изображения_2',
-      description: 'описание_изображения_2',
-      likes: 20,
-      comments: 7
-    }
-  ];
 
-  // Функция для создания DOM-элементов изображений
-  const createPictureElement = (pictureData) => {
-    const pictureTemplate = document.querySelector('#picture');
-    const pictureClone = pictureTemplate.content.cloneNode(true);
+import { generatePhotosArray } from './data.js';
 
-    pictureClone.querySelector('.picture__img').setAttribute('src', pictureData.url);
-    pictureClone.querySelector('.picture__img').setAttribute('alt', pictureData.description);
-    pictureClone.querySelector('.picture__likes').textContent = pictureData.likes;
-    pictureClone.querySelector('.picture__comments').textContent = pictureData.comments;
+// Функция для создания DOM-элементов изображений
+export const createPictureElement = (pictureData) => {
+  const pictureTemplate = document.querySelector('#picture');
+  const pictureClone = pictureTemplate.content.cloneNode(true);
 
-    return pictureClone;
-  };
+  pictureClone.querySelector('.picture__img').setAttribute('src', pictureData.url);
+  pictureClone.querySelector('.picture__img').setAttribute('alt', pictureData.description);
+  pictureClone.querySelector('.picture__likes').textContent = pictureData.likes;
+  pictureClone.querySelector('.picture__comments').textContent = pictureData.comments;
 
-  const renderPictures = () => {
-    const fragment = new DocumentFragment();
+  return pictureClone;
+};
 
-    picturesData.forEach((data) => {
-      const pictureElement = createPictureElement(data);
-      fragment.appendChild(pictureElement);
-    });
+const renderPictures = () => {
+  const fragment = new DocumentFragment();
+  const picturesData = generatePhotosArray();
 
-    document.querySelector('.pictures').appendChild(fragment);
-  };
+  picturesData.forEach((data) => {
+    const pictureElement = createPictureElement(data);
+    fragment.appendChild(pictureElement);
+  });
 
-  return {
-    renderPictures: renderPictures
-  };
-})();
+  document.querySelector('.pictures').appendChild(fragment);
+};
 
-picturesModule.renderPictures();
+renderPictures();
